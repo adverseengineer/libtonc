56aa889fc1a681816896e5911ebd03bf0a7a0eba
Nick <adverseengineer@gmail.com>
2021-02-25 07:33:40 -0500

    fixed wonky indentation

--- include/tonc_mgba.h
+++ include/tonc_mgba.h
@@ -10,14 +10,14 @@
 
 #include "tonc_types.h"
 
-#define LOG_FATAL				(u32) 0x100
-#define LOG_ERR					(u32) 0x101
-#define LOG_WARN				(u32) 0x102
-#define LOG_INFO				(u32) 0x103
-#define LOG_MAX_CHARS_PER_LINE	(u32) 256
-#define REG_LOG_STR				(char*) 0x4FFF600
-#define REG_LOG_LEVEL			*(vu16*) 0x4FFF700
-#define REG_LOG_ENABLE  		*(vu16*) 0x4FFF780
+#define LOG_FATAL               (u32) 0x100
+#define LOG_ERR                 (u32) 0x101
+#define LOG_WARN                (u32) 0x102
+#define LOG_INFO                (u32) 0x103
+#define LOG_MAX_CHARS_PER_LINE  (u32) 256
+#define REG_LOG_STR             (char*) 0x4FFF600
+#define REG_LOG_LEVEL           *(vu16*) 0x4FFF700
+#define REG_LOG_ENABLE          *(vu16*) 0x4FFF780
 
 void mgba_log(const u32 level, const char* str);
 void mgba_printf(const u32 level, const char* str, ...);
