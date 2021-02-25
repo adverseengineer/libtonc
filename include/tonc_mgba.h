e1e4b53f7815a8ad9c8aa977b20270d49ae4f9ee
Nick <adverseengineer@gmail.com>
2021-02-25 07:26:47 -0500

    added mgba_printf

--- include/tonc_mgba.h
+++ include/tonc_mgba.h
@@ -10,15 +10,16 @@
 
 #include "tonc_types.h"
 
-#define REG_LOG_STR      (char*) 0x4FFF600
-#define REG_LOG_LEVEL   *(vu16*) 0x4FFF700
-#define REG_LOG_ENABLE  *(vu16*) 0x4FFF780
+#define LOG_FATAL				(u32) 0x100
+#define LOG_ERR					(u32) 0x101
+#define LOG_WARN				(u32) 0x102
+#define LOG_INFO				(u32) 0x103
+#define LOG_MAX_CHARS_PER_LINE	(u32) 256
+#define REG_LOG_STR				(char*) 0x4FFF600
+#define REG_LOG_LEVEL			*(vu16*) 0x4FFF700
+#define REG_LOG_ENABLE  		*(vu16*) 0x4FFF780
 
-#define LOG_FATAL       0x100
-#define LOG_ERR         0x101
-#define LOG_WARN        0x102
-#define LOG_INFO        0x103
-
-void mgba_log(const char* str, const u32 level);
+void mgba_log(const u32 level, const char* str);
+void mgba_printf(const u32 level, const char* str, ...);
 
 #endif
--- src/tonc_mgba.c
+++ src/tonc_mgba.c
@@ -5,23 +5,35 @@
 //! \author Nick Sells
 //! \date 20210221
 
+#include <stdarg.h>
+#include <stdio.h>
 #include <string.h>
 #include "tonc_math.h"
 #include "tonc_mgba.h"
 
 //adapted from https://github.com/GValiente/butano/blob/8017ce68628a86d8eb26b5077b84346c3143b54e/butano/hw/src/bn_hw_log.cpp#L22
-void mgba_log(const char* str, const u32 level) {
+void mgba_log(const u32 level, const char* str) {
     REG_LOG_ENABLE = 0xC0DE;
-    const u32 max_chars_per_line = 256; //not arbitrary. the register is just 256 chars wide
     u32 chars_left = strlen(str);
 
-    while(chars_left) { //breaks the message into 256-char log entries
-        u32 chars_to_write = min(chars_left, max_chars_per_line);
+    while(chars_left) { //splits the message into 256-char log entries
+        u32 chars_to_write = min(chars_left, LOG_MAX_CHARS_PER_LINE);
 
         memcpy(REG_LOG_STR, str, chars_to_write);
         REG_LOG_LEVEL = level; //every time this is written to, mgba creates a new log entry
 
-        str += chars_to_write; //increments the POINTER to the string, took me forever to realize
+        str += chars_to_write;
         chars_left -= chars_to_write;
     }
+}
+
+void mgba_printf(const u32 level, const char* fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+
+	char buf[LOG_MAX_CHARS_PER_LINE];
+	vsprintf(buf, fmt, args);
+	mgba_log(level, buf);
+
+	va_end(args);
 }
