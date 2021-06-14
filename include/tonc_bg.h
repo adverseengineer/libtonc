b98f291e43b4bfd27e285cd3a1d854c81a873f41
Nick <adverseengineer@gmail.com>
2021-06-14 03:02:06 -0400

    massively simplified tonc_mgba.h
    jesus christ i'm stupid

--- include/tonc_bg.h
+++ include/tonc_bg.h
@@ -13,68 +13,72 @@
 #include "tonc_video.h"
 
 // --- single-tile routines ---
-INLINE void bg_fill(int sbb, SCR_ENTRY se);
-INLINE void bg_plot(int sbb, int x, int y, SCR_ENTRY se);
-INLINE void bg_hline(int sbb, int x0, int x1, int y, SCR_ENTRY se);
-INLINE void bg_vline(int sbb, int x, int y0, int y1, SCR_ENTRY se);
+INLINE void bg_fill(u32 sbb, SCR_ENTRY se);
+INLINE void bg_plot(u32 sbb, u32 x, u32 y, SCR_ENTRY se);
+INLINE void bg_hline(u32 sbb, u32 x0, u32 x1, u32 y, SCR_ENTRY se);
+INLINE void bg_vline(u32 sbb, u32 x, u32 y0, u32 y1, SCR_ENTRY se);
 
-INLINE void bg_rect(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se);
-INLINE void bg_frame(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se);
-INLINE void bg_window(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0);
+INLINE void bg_rect(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se);
+INLINE void bg_frame(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se);
+INLINE void bg_window(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0);
 
 // --- meta-tile variants ---
-INLINE void bg_fill_m(int sbb, SCR_ENTRY se0);
-INLINE void bg_plot_m(int sbb, int x, int y, SCR_ENTRY se0);
-INLINE void bg_hline_m(int sbb, int x0, int x1, int y, SCR_ENTRY se0);
-INLINE void bg_vline_m(int sbb, int x, int y0, int y1, SCR_ENTRY se0);
+INLINE void bg_fill_m(u32 sbb, SCR_ENTRY se0);
+INLINE void bg_plot_m(u32 sbb, u32 x, u32 y, SCR_ENTRY se0);
+INLINE void bg_hline_m(u32 sbb, u32 x0, u32 x1, u32 y, SCR_ENTRY se0);
+INLINE void bg_vline_m(u32 sbb, u32 x, u32 y0, u32 y1, SCR_ENTRY se0);
 
-INLINE void bg_rect_m(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0);
-INLINE void bg_frame_m(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0);
-INLINE void bg_window_m(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0);
+INLINE void bg_rect_m(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0);
+INLINE void bg_frame_m(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0);
+INLINE void bg_window_m(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0);
 
 
 
 //! Fill screenblock \a sbb with \a se
-INLINE void bg_fill(int sbb, SCR_ENTRY se)
+INLINE void bg_fill(u32 sbb, SCR_ENTRY se)
 {
 	memset32(se_mem[sbb], dup16(se), SBB_SIZE / 4);
 }
 
 //! Plot a screen entry at (\a x,\a y) of screenblock \a sbb.
 //! NOTE: treats four 32x32 screenblocks as a single 64x64 one
-INLINE void bg_plot(int sbb, int x, int y, SCR_ENTRY se)
+INLINE void bg_plot(u32 sbb, u32 x, u32 y, SCR_ENTRY se)
 {
-	se_mem[sbb][y*32+x+((x>=32)*992)+((y>=32)*1024)] = se;
+	u32 bottom = (y & 32) ? 2048 : 0;
+	u32 right = (x & 32) ? 1024 : 0;
+	u32 row = y & 31;
+	u32 col = x & 31;
+	se_mem[sbb][col + row * 32 + bottom + right] = se;
 }
 
 //! Plot a horizontal line of tiles on screenblock \a sbb
-INLINE void bg_hline(int sbb, int x0, int x1, int y, SCR_ENTRY se)
+INLINE void bg_hline(u32 sbb, u32 x0, u32 x1, u32 y, SCR_ENTRY se)
 {
-	for(int ix = x0; ix < x1; ix++)
+	for(u32 ix = x0; ix < x1; ix++)
 		bg_plot(sbb, ix, y, se);
 }
 
 //! Plot a vertical line of tiles on screenblock \a sbb
-INLINE void bg_vline(int sbb, int x, int y0, int y1, SCR_ENTRY se)
+INLINE void bg_vline(u32 sbb, u32 x, u32 y0, u32 y1, SCR_ENTRY se)
 {
-	for(int iy = y0; iy < y1; iy++)
+	for(u32 iy = y0; iy < y1; iy++)
 		bg_plot(sbb, x, iy, se);
 }
 
 //! Fill a rectangle on \a sbb with \a se.
-INLINE void bg_rect(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se)
+INLINE void bg_rect(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se)
 {
 	bmp16_rect(left, top, right, bottom, se, se_mem[sbb], 32*2);
 }
 
 //! Create a border on \a sbb with \a se.
-INLINE void bg_frame(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se)
+INLINE void bg_frame(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se)
 {
 	bmp16_frame(left, top, right, bottom, se, se_mem[sbb], 32*2);
 }
 
 //! Create a framed rectangle on \a sbb with the nine tiles starting at \a se0
-INLINE void bg_window(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0)
+INLINE void bg_window(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0)
 {
 	//corners
 	bg_plot(sbb, left, top, se0);
@@ -92,13 +96,13 @@ INLINE void bg_window(int sbb, int left, int top, int right, int bottom, SCR_ENT
 }
 
 //! Fill screenblock \a sbb with the meta-tile starting at \a s0
-INLINE void bg_fill_m(int sbb, SCR_ENTRY se0) {
-	for(int iy = 0; iy < 16; iy++)
+INLINE void bg_fill_m(u32 sbb, SCR_ENTRY se0) {
+	for(u32 iy = 0; iy < 16; iy++)
 		bg_hline_m(sbb, 0, 15, iy, se0);
 }
 
 //! Plot the meta-tile starting at \a se0 at (\a x,\a y) of screenblock \a sbb
-INLINE void bg_plot_m(int sbb, int x, int y, SCR_ENTRY se0)
+INLINE void bg_plot_m(u32 sbb, u32 x, u32 y, SCR_ENTRY se0)
 {
 	bg_plot(sbb, x * 2, y * 2, se0);
 	bg_plot(sbb, x * 2 + 1, y * 2, se0 + 1);
@@ -107,28 +111,28 @@ INLINE void bg_plot_m(int sbb, int x, int y, SCR_ENTRY se0)
 }
 
 //! Plot a horizontal line on screenblock \a sbb with the meta-tile starting at \a se0
-INLINE void bg_hline_m(int sbb, int x0, int x1, int y, SCR_ENTRY se0)
+INLINE void bg_hline_m(u32 sbb, u32 x0, u32 x1, u32 y, SCR_ENTRY se0)
 {
-	for(int ix = x0; ix <= x1; ix++)
+	for(u32 ix = x0; ix <= x1; ix++)
 		bg_plot_m(sbb, ix, y, se0);
 }
 
 //! Plot a vertical line on screenblock \a sbb with the meta-tile starting at \a se0
-INLINE void bg_vline_m(int sbb, int x, int y0, int y1, SCR_ENTRY se0)
+INLINE void bg_vline_m(u32 sbb, u32 x, u32 y0, u32 y1, SCR_ENTRY se0)
 {
-	for(int iy = y0; iy <= y1; iy++)
+	for(u32 iy = y0; iy <= y1; iy++)
 		bg_plot_m(sbb, x, iy, se0);
 }
 
 //! Fill a rectangle on \a sbb with the meta-tile starting at \a se0
-INLINE void bg_rect_m(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0)
+INLINE void bg_rect_m(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0)
 {
-	for(int iy = top; iy <= bottom; iy++)
+	for(u32 iy = top; iy <= bottom; iy++)
 		bg_hline_m(sbb, left, right, iy, se0);
 }
 
 //! Create a border on \a sbb with the meta-tile starting at \a se0
-INLINE void bg_frame_m(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0)
+INLINE void bg_frame_m(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0)
 {
 	bg_hline_m(sbb, left, right, top, se0);
 	bg_hline_m(sbb, left, right, bottom, se0);
@@ -137,7 +141,7 @@ INLINE void bg_frame_m(int sbb, int left, int top, int right, int bottom, SCR_EN
 }
 
 //! Create a framed rectangle on \a sbb with the nine meta-tiles starting at \a se0
-INLINE void bg_window_m(int sbb, int left, int top, int right, int bottom, SCR_ENTRY se0)
+INLINE void bg_window_m(u32 sbb, u32 left, u32 top, u32 right, u32 bottom, SCR_ENTRY se0)
 {
 	//corners
 	bg_plot_m(sbb, left, top, se0);
--- include/tonc_mgba.h
+++ include/tonc_mgba.h
@@ -9,32 +9,46 @@
 #ifndef TONC_MGBA
 #define TONC_MGBA
 
+
+#include <stdarg.h>
+#include <stdio.h>
 #include "tonc_types.h"
+
+// --------------------------------------------------------------------
+// STRUCTS
+// --------------------------------------------------------------------
+
+typedef enum LogLevel {
+	LOG_FATAL                   = 0x100,
+	LOG_ERR                     = 0x101,
+	LOG_WARN                    = 0x102,
+	LOG_INFO                    = 0x103
+} LogLevel;
 
 // --------------------------------------------------------------------
 // MACROS
 // --------------------------------------------------------------------
 
-#define LOG_FATAL               (u32) 0x100
-#define LOG_ERR                 (u32) 0x101
-#define LOG_WARN                (u32) 0x102
-#define LOG_INFO                (u32) 0x103
-
-#define LOG_MAX_CHARS_PER_LINE  (u32) 256
-#define REG_LOG_STR             (char*) 0x4FFF600
-#define REG_LOG_LEVEL           *(vu16*) 0x4FFF700
 #define REG_LOG_ENABLE          *(vu16*) 0x4FFF780
-
-//shorthand log and printf because specifying a log level gets really old really fast
-#define log(str)                mgba_log(LOG_INFO, (str))
-#define printf(str, args...)    mgba_printf(LOG_INFO, (str), (args));
+#define REG_LOG_LEVEL           *(vu16*) 0x4FFF700
 
 // --------------------------------------------------------------------
-// PROTOTYPES
+// INLINES
 // --------------------------------------------------------------------
 
-void mgba_log(u32 level, const char* str);
-void mgba_printf(u32 level, const char* str, ...);
+//! Outputs \a fmt formatted with varargs to mGBA's logger with \a level priority
+static inline void mgba_printf(LogLevel lvl, const char* fmt, ...) {
+	REG_LOG_ENABLE = 0xC0DE;
+	REG_LOG_LEVEL = lvl;
+
+	va_list args;
+	va_start(args, fmt);
+
+	char* const log = (char*) 0x4FFF600;
+	vsnprintf(log, 0x100, fmt, args);
+
+	va_end(args);
+}
 
 #endif // TONC_MGBA
 
